e287d765638ff8e1f08ac6519fce35473e02c785
MattsKeys <matias.ambort@gmail.com>
2026-05-31 16:26:08 -0300

    libreria cowsay y txt con requirements

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hola Mundo, ésta es mi PITÓN")
+import cowsay
+
+#print("Hola Mundo, ésta es mi PITÓN")
+#cowsay.cow("Hola Mundo, ésta es mi PITÓN")
+
+def main():
+    # TODO: Invoca aquí la función de cowsay con el mensaje que desees
+    cowsay.cow("Hola Mundo, ésta es mi PITÓN")
+
+if __name__ == "__main__":
+    main()
