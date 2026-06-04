6627e1b2295dfc4b8ba44ab453b59ee83b7d06a2
MNMansilla438 <marcosmansill4@gmail.com>
2026-06-04 19:33:39 -0300

    A CASA HIROSHIMA

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def menu_principal():
     print("1. Ejecutar procesamiento de transacciones")
     print("2. Mostrar mensajes de cowsay")
     print("3. Enviar Prompt")
-    #print("3/. Enviar Prompt")
+    print("4. Puti Prompt")
     print("x. Salir")
     
     opcion = input("Seleccione una opción: ")
